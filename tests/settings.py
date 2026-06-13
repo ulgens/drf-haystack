e81363c63c2e96fc660ebea2634983fac6603916
Ülgen Sarıkavak <ulgensrkvk@gmail.com>
2026-06-13 11:11:48 +0300

    Add trailing comma for better readability

--- tests/settings.py
+++ tests/settings.py
@@ -7,7 +7,12 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
-DATABASES = {"default": {"ENGINE": "django.db.backends.sqlite3", "NAME": os.path.join(BASE_DIR, os.pardir, "test.db")}}
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": os.path.join(BASE_DIR, os.pardir, "test.db"),
+    }
+}
 
 INSTALLED_APPS = (
     "django.contrib.auth",
