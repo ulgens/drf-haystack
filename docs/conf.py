0fde3f41029bb4ce026774955f297716c1e58473
Ülgen Sarıkavak <ulgensrkvk@gmail.com>
2026-08-04 13:21:23 +0300

    Remove unused conf option

--- docs/conf.py
+++ docs/conf.py
@@ -19,10 +19,6 @@ django.setup()
 
 # -- General configuration ------------------------------------------------
 
-# If your documentation needs a minimal Sphinx version, state it here.
-#
-# needs_sphinx = '1.0'
-
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
